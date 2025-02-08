09570737a5cfdbe8c89d25e89023d259227a99d3
Ferhat Sezgin <ziyaretli.ferhat.56@gmail.com>
2025-02-08 16:07:45 +0300

    header completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-
+import Header from "./components/header"
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+   <Header/>
+    </div>
   )
 }
 
---
+++ src/components/header.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <div className="headerContanier">
+      <img
+        className="logo"
+        src="https://ajansara.com/wp-content/uploads/Wolswagen-yeni-logo-1024x1024.png"
+        alt="logo"
+      />
+      <ul className="list">
+        <li>ANASAYFA</li>
+        <li>İLETİŞİM</li>
+        <li>BİLGİ</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
